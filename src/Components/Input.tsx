0cc3962359477d71642cc899516ab2e92ab9355a
Winndie Fan <winndie@gmail.com>
2023-10-24 16:36:45 +0100

    added uiKey

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -5,11 +5,11 @@ import { RootState, useAppDispatch } from "../state"
 import { updateError, updateMission } from "../state/missions"
 import { IMission, InputProps } from "../types"
 
-const Input:React.FC<{name:keyof IMission,props:InputProps}> = ({name,props}) => {
+const Input:React.FC<{uiKey:keyof IMission,props:InputProps}> = ({uiKey,props}) => {
   
   const dispatch = useAppDispatch()
-  const value = useSelector((state:RootState) => state.mission.item[name])
-  const error = useSelector((state:RootState) => state.mission.errors[name])
+  const value = useSelector((state:RootState) => state.mission.item[uiKey])
+  const error = useSelector((state:RootState) => state.mission.errors[uiKey])
 
   return (<>
     <div className="col-sm-2 col-md-2 col-lg-2 col-xl-2">
@@ -18,8 +18,8 @@ const Input:React.FC<{name:keyof IMission,props:InputProps}> = ({name,props}) =>
     <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
     <input
         value={value}
-        onFocus={()=>dispatch(updateError({key:name, error:undefined}))}
-        onChange={(e)=>dispatch(updateMission({key:name, value:e.currentTarget.value}))}
+        onFocus={()=>dispatch(updateError({key:uiKey, error:undefined}))}
+        onChange={(e)=>dispatch(updateMission({key:uiKey, value:e.currentTarget.value}))}
         placeholder={props.placeholder}
         minLength={props.minLength}
         maxLength={props.maxLength}
